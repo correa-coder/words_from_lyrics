90c4c1e9b4137baf17585cb345a60afd7b37d939
Guilherme Correa <80556636+gui-correa-coder@users.noreply.github.com>
2022-01-20 02:00:46 -0300

    function to load txt files, closes #3

--- module.py
+++ module.py
@@ -4,3 +4,14 @@ import os
 def filename_from_path(path:str):
     return os.path.split(path)[-1]
 
+
+def load_txt(filename: str):
+    content = 'ERROR: Only .txt files allowed'
+
+    if filename.endswith('.txt'):
+        fp = os.path.join(os.path.dirname(__file__), filename)
+
+        with open(fp, 'r', encoding='utf-8') as f:
+            content = f.read()
+
+    return content
